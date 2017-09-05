9f1c7e64bc327b405f255a730f7898249db6d1a9
Jagreaper <jamesalexgrant@hotmail.com>
2017-09-05 15:42:08 +1000

    Added GLFW and GLEW Imports for Kaleid.Graphics.OpenGL

--- Src/stdafx.hpp
+++ Src/stdafx.hpp
@@ -5,7 +5,18 @@
 #endif
 
 #ifdef KALEID_GRAPHICS_OPENGL_DLL
+
+#include <GLEW/glew.h> // include GLEW and new version of GL on Windows
+#define GLFW_DLL
+#include <GLFW/glfw3.h> // GLFW helper library
+
 #define KALEID_GRAPHICS_OPENGL_API __declspec(dllexport)
 #else
 #define KALEID_GRAPHICS_OPENGL_API __declspec(dllimport)
+#endif
+
+#ifdef KALEID_GAME_DLL
+#define KALEID_GAME_API __declspec(dllexport)
+#else
+#define KALEID_GAME_API __declspec(dllimport)
 #endif
