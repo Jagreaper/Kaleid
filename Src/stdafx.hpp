1f7c1b767ca89eddf81d06ac80b6d89e197911ce
Jagreaper <jamesalexgrant@hotmail.com>
2017-09-06 06:46:08 +1000

    Moved OpenGL implementation to Graphics Project

--- Src/stdafx.hpp
+++ Src/stdafx.hpp
@@ -1,20 +1,14 @@
 #pragma once
 
 #ifdef KALEID_GRAPHICS_DLL
+
+#include <GLEW/glew.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+
 #define KALEID_GRAPHICS_API __declspec(dllexport)
 #else
 #define KALEID_GRAPHICS_API __declspec(dllimport)
-#endif
-
-#ifdef KALEID_GRAPHICS_OPENGL_DLL
-
-#include <GLEW/glew.h> // include GLEW and new version of GL on Windows
-#define GLFW_DLL
-#include <GLFW/glfw3.h> // GLFW helper library
-
-#define KALEID_GRAPHICS_OPENGL_API __declspec(dllexport)
-#else
-#define KALEID_GRAPHICS_OPENGL_API __declspec(dllimport)
 #endif
 
 #ifdef KALEID_GAME_DLL
