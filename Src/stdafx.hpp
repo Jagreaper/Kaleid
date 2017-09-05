f9eab5f3279bf7bbf956b9013f0fedfd9cac52b6
Jagreaper <jamesalexgrant@hotmail.com>
2017-09-05 14:18:27 +1000

    Added KALEID_GRAPHICS_OPENGL_API definition

--- Src/stdafx.hpp
+++ Src/stdafx.hpp
@@ -3,3 +3,9 @@
 #else
 #define KALEID_GRAPHICS_API __declspec(dllimport);
 #endif
+
+#ifdef KALEID_GRAPHICS_OPENGL_DLL
+#define KALEID_GRAPHICS_OPENGL_API __declspec(dllexport);
+#else
+#define KALEID_GRAPHICS_OPENGL_API __declspec(dllimport);
+#endif
