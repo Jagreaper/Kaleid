66494301f59353afb7a7fd38b0f089b4b13b7805
Jagreaper <jamesalexgrant@hotmail.com>
2017-09-20 15:39:14 +1000

    Added support for ObjStreamDecoder to read verticies, normals and texels into memory

--- Src/IO/ObjStreamDecoder.cpp
+++ Src/IO/ObjStreamDecoder.cpp
@@ -3,14 +3,21 @@
 #include "GraphicsFactory.hpp"
 #include "Model.hpp"
 #include "StringHelper.hpp"
+#include "Vertex.hpp"
 #include <string>
 
 using namespace Kaleid::IO;
+using namespace Kaleid::Math;
+using namespace Kaleid::Geometry;
 using namespace Kaleid::Graphics;
 using namespace Kaleid::Helpers;
 
 bool ObjStreamDecoder::TryDecode(std::istream& source, Kaleid::Game::Model* output, Kaleid::Graphics::GraphicsFactory* arg)
 {
+	std::vector<Vector3f> verticies;
+	std::vector<Vector3f> normals;
+	std::vector<Vector2f> texels;
+
 	std::string line;
 	while (std::getline(source, line))
 	{
@@ -18,8 +25,24 @@ bool ObjStreamDecoder::TryDecode(std::istream& source, Kaleid::Game::Model* outp
 		{
 			std::string nline = line.substr(2, line.size() - 2);
 			std::vector<std::string> strings = StringHelper::Split(nline, ' ');
+			verticies.push_back(Vector3f(std::stof(strings[0]), std::stof(strings[1]), std::stof(strings[2])));
+			continue;
+		}
 
-			// Verticies
+		if (StringHelper::BeginsWith(line, std::string("vn ")))
+		{
+			std::string nline = line.substr(3, line.size() - 3);
+			std::vector<std::string> strings = StringHelper::Split(nline, ' ');
+			normals.push_back(Vector3f(std::stof(strings[0]), std::stof(strings[1]), std::stof(strings[2])));
+			continue;
+		}
+
+		if (StringHelper::BeginsWith(line, std::string("vt ")))
+		{
+			std::string nline = line.substr(3, line.size() - 3);
+			std::vector<std::string> strings = StringHelper::Split(nline, ' ');
+			texels.push_back(Vector2f(std::stof(strings[0]), std::stof(strings[1])));
+			continue;
 		}
 	}
 
