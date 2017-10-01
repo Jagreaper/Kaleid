81e4383a52036b0b90fd04ba9de45b321ac73809
Jagreaper <jamesalexgrant@hotmail.com>
2017-10-02 01:14:58 +1000

    Added Kaleid.Input Project

--- Src/Shared/stdafx.hpp
+++ Src/Shared/stdafx.hpp
@@ -40,45 +40,11 @@
 	#else
 		#define KALEID_GEOMETRY_API __declspec(dllimport)
 	#endif
-#else
-	#ifdef KALEID_GRAPHICS_DLL
-		#include <GLEW/glew.h>
-		#define GLFW_DLL
-		#include <GLFW/glfw3.h>
 
-		#define KALEID_GRAPHICS_API __attribute__((visibility("default")))
+	#ifdef KALEID_INPUT_DLL
+		#define KALEID_INPUT_API __declspec(dllexport)
 	#else
-		#define KALEID_GRAPHICS_API extern
-	#endif
-
-	#ifdef KALEID_GAME_DLL
-		#define KALEID_GAME_API __attribute__((visibility("default")))
-	#else
-		#define KALEID_GAME_API extern
-	#endif
-
-	#ifdef KALEID_MATH_DLL
-		#define KALEID_MATH_API __attribute__((visibility("default")))
-	#else
-		#define KALEID_MATH_API extern
-	#endif
-
-	#ifdef KALEID_HELPERS_DLL
-		#define KALEID_HELPERS_API __attribute__((visibility("default")))
-	#else
-		#define KALEID_HELPERS_API extern
-	#endif
-
-	#ifdef KALEID_IO_DLL
-		#define KALEID_IO_API __attribute__((visibility("default")))
-	#else
-		#define KALEID_IO_API extern
-	#endif
-
-	#ifdef KALEID_GEOMETRY_DLL
-		#define KALEID_GEOMETRY_API __attribute__((visibility("default")))
-	#else
-		#define KALEID_GEOMETRY_API extern
+		#define KALEID_INPUT_API __declspec(dllimport)
 	#endif
 #endif
 
