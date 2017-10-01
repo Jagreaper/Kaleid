01df19cbd8fdff2cd4458ea3c7f4ad6e0b451125
Jagreaper <jamesalexgrant@hotmail.com>
2017-10-01 23:18:11 +1000

    Fixed IndexBuffer TypeInfo error

--- Src/Graphics/IndexBuffer.cpp
+++ Src/Graphics/IndexBuffer.cpp
@@ -14,13 +14,13 @@ void IndexBuffer::Bind()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->_id);
 }
 
-#define INDEX_BUFFER_DATA_SETTER(TYPE, GL_TYPE) \
+#define INDEX_BUFFER_DATA_SETTER(TYPE, OGL_TYPE) \
 void IndexBuffer::BufferData(const TYPE* data, size_t count, BufferUsage usage) \
 { \
 	this->Bind(); \
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(TYPE), data, usage); \
 	this->_length = count; \
-	int _type_info = GL_TYPE; \
+	this->_type_info = OGL_TYPE; \
 } \
 \
 void IndexBuffer::BufferData(const std::vector<TYPE>* data, BufferUsage usage) \
@@ -28,7 +28,7 @@ void IndexBuffer::BufferData(const std::vector<TYPE>* data, BufferUsage usage) \
 	this->Bind(); \
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, data->size() * sizeof(TYPE), data, usage); \
 	this->_length = data->size(); \
-	int _type_info = GL_TYPE; \
+	this->_type_info = OGL_TYPE; \
 } \
 
 INDEX_BUFFER_DATA_SETTER(unsigned char, GL_UNSIGNED_BYTE)
